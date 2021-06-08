949a38cbd13ea01542737a1c2ea47c0fcb8aa157
Gabriel <bielmarques922@gmail.com>
2021-06-08 09:45:29 -0300

    Melhorando o Header o Footer

---
+++ footer.php
@@ -0,0 +1,79 @@
+<section class="break">
+    <blockquote class="quote-external container">
+      <p>
+        "o verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana"
+      </p>
+      <cite>WILLIAM MORRIS</cite>
+    </blockquote>
+  </section>
+
+<!-- FOOTER -->
+<footer>
+    <div class="footer">
+      <div class="container">
+
+        <!-- About Us -->
+        <div class="grid-8 footer_about">
+          <h3>Nossa História</h3>
+          <p>
+            o verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana.
+            Interrese por todos os detalher da vida cotidiana.
+          </p>
+        </div>
+
+        <!-- Contact -->
+        <div class="grid-4 footer_contact">
+          <h3>Contato</h3>
+          <ul>
+            <li>- 21 23232-4343</li>
+            <li>- contato@bikcraft.com</li>
+            <li>- Botafogo - RJ</li>
+          </ul>
+        </div>
+
+        <!-- Social Medias -->
+        <div class="grid-4 footer_midias">
+          <h3>Contato</h3>
+          <ul>
+            <!-- Facebook -->
+            <li>
+              <a href="http://facebook.com" target="_blank">
+                <img src="img/redes-sociais/facebook.svg" alt="Facebook">
+              </a>
+            </li>
+
+            <!-- Instagram -->
+            <li>
+              <a href="http://instagram.com" target="_blank">
+                <img src="img/redes-sociais/instagram.svg" alt="Instagram">
+              </a>
+            </li>
+
+            <!-- Twitter -->
+            <li>
+              <a href="http://twitter.com" target="_blank">
+                <img src="img/redes-sociais/twitter.svg" alt="Twitter">
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+
+    <!-- COPYRIGHT -->
+    <div class="copy">
+      <div class="container">
+        <p class="grid-16">Bikcraft 2015 - Alguns direitos reservados.</p>
+      </div>
+    </div>
+  </footer>
+
+  <script src="./js/simple-anime.js"></script>
+  <script src="./js/script.js"></script>
+
+  <!-- INICIO WORDPRESS FOOTER-->
+  <?php wp_footer(); ?>
+  <!-- FINAL WORDPRESS FOOTER-->
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php bloginfo('name'); ?></title>
+
+  <!-- Tags de SEO-->
+  <meta name="description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte.">
+  <meta property="og:type" content="website"/>
+  <meta property="og:title" content="<?php bloginfo('name'); ?>"/>
+  <meta property="og:description" content="Compre a sua bicicleta personalizada na Bikcraft. Possuímos modelos Passeio, Retrô e Esporte."/>
+  <meta property="og:url" content="http://bikcraft.com"/>
+  <meta property="og:image" content="http://bikcraft.com/img/og-image.png"/>
+
+
+  <!-- Estilos CSS-->
+  <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+  
+  <link rel="shortcut icon" href="./img/favicon.ico">
+
+   <!-- SCRIPTS-->
+    <script>
+      document.documentElement.classList.add("js");
+    </script>
+
+<!-- INICIO WORDPRESS HEADER-->
+<?php wp_head(); ?>
+<!-- FINAL WORDPRESS HEADER-->
+</head>
+<body>
+
+  <!-- HEADER -->
+  <header class="header">
+    <div class="container">
+      <a href="index.html" class="grid-4">
+        <img src="img/bikcraft.svg" alt="Bikcraft">
+      </a>
+      <nav class="grid-12 header_menu">
+        <ul>
+          <li><a href="index.html" class="active_menu">Home</a></li>
+          <li><a href="about.html">Sobre</a></li>
+          <li><a href="products.html">Produtos</a></li>
+          <li><a href="portfolio.html">Portfólio</a></li>
+          <li><a href="contact.html">Contato</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
