1bae68c9e20a6a42fa7dd64031dddc190bae0fb6
Gabriel <bielmarques922@gmail.com>
2021-06-30 09:48:42 -0300

    Portfólio

--- inc/clientes-portfolio.php
+++ inc/clientes-portfolio.php
@@ -11,17 +11,6 @@
 
 	<?php endwhile; else: endif; ?>
 
-	<!-- <li>
-		<div class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-		<div class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-		<div class="grid-16"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-	</li>
-
-	<li>
-		<div class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/passeio.jpg" alt="Bicicleta Passeio"></div>
-		<div class="grid-8"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/retro.jpg" alt="Bicicleta Retrô"></div>
-		<div class="grid-16"><img src="<?php echo get_template_directory_uri(); ?>/img/portfolio/esporte.jpg" alt="Bicicleta Esporte"></div>
-	</li> -->
 </ul>
 
 <?php if (!is_page('portfolio')) { ?>
